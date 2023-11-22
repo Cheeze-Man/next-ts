c2fce811fb5e9a20ac2e6c2162d343575be31fe2
cheezeman <pksam5046@naver.com>
2023-11-22 13:39:38 +0900

    edit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="bg-white p-5">
+          <Link className="mr-10 text-black" href="/">
+            Home
+          </Link>
+          <Link className="mr-10 text-black" href="/list">
+            List
+          </Link>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
