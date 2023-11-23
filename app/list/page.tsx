7178d39575ba3ea72ce027edce6e9322f119e0e7
cheezeman <pksam5046@naver.com>
2023-11-23 19:54:54 +0900

    wip

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,11 +1,17 @@
+import { PRODUCT_LIST } from "@/public/util/constant";
+
 export default function List() {
-  let products: string[] = ["Tomatoes", "Pasta", "Coconut"];
   return (
     <div>
       <h4 className="text-center">상품목록</h4>
-      {products.map((product) => (
-        <div className="my-20 mx-auto w-1/6 bg-white text-black p-20 rounded-md">
-          <h4 className="font-bold text-lg">{product} $40</h4>
+      {PRODUCT_LIST.map(({ name, price }) => (
+        <div
+          className="my-20 mx-auto w-1/6 bg-white text-black p-20 rounded-md"
+          key={name}
+        >
+          <h4 className="font-bold text-lg">
+            {name} ${price}
+          </h4>
         </div>
       ))}
     </div>
---
+++ public/util/constant.ts
@@ -0,0 +1,18 @@
+interface Product {
+  name: string;
+  price: number;
+}
+export const PRODUCT_LIST: Product[] = [
+  {
+    name: "Tomatoes",
+    price: 40,
+  },
+  {
+    name: "Pasta",
+    price: 50,
+  },
+  {
+    name: "Coconut",
+    price: 70,
+  },
+];
