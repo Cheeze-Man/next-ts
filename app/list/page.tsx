afd076dd6e210ed958b63ed14a5765c7bded4709
cheezeman <pksam5046@naver.com>
2023-11-23 19:41:39 +0900

    wip

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,13 +1,13 @@
 export default function List() {
+  let products: string[] = ["Tomatoes", "Pasta", "Coconut"];
   return (
     <div>
       <h4 className="text-center">상품목록</h4>
-      <div className="my-20 mx-auto w-1/6 bg-white text-black p-20 rounded-md">
-        <h4 className="font-bold text-lg">상품1 &40</h4>
-      </div>
-      <div className="my-20 mx-auto w-1/6 bg-white text-black p-20 rounded-md">
-        <h4 className="font-bold text-lg">상품2 &40</h4>
-      </div>
+      {products.map((product) => (
+        <div className="my-20 mx-auto w-1/6 bg-white text-black p-20 rounded-md">
+          <h4 className="font-bold text-lg">{product} $40</h4>
+        </div>
+      ))}
     </div>
   );
 }
