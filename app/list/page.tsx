896f7b854c0caf1c94e3381bb2b6cf0fd5224ba6
cheezeman <pksam5046@naver.com>
2023-11-27 20:15:40 +0900

    WIP : List컴포넌트 내 handleAmountChange 함수 버그 fix 필요

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import { PRODUCT_LIST } from "@/public/util/constant";
+import { useState } from "react";
 
 export default function List() {
+  const [listData, setListData] = useState(PRODUCT_LIST);
+
+  const handleAmountChange = (i: number, amountChange: number): void => {
+    setListData((prev) => {
+      const updatedListData = [...prev];
+      const newAmount = updatedListData[i].amount + amountChange;
+      updatedListData[i].amount = Math.max(0, newAmount);
+      return updatedListData;
+    });
+  };
+
   return (
     <div>
       <h4 className="text-center">상품목록</h4>
-      {PRODUCT_LIST.map(({ name, price }, i) => (
+      {listData.map(({ name, price, amount }, i) => (
         <div
           className="my-20 mx-auto w-1/6 bg-white text-black p-20 rounded-md"
           key={name + i}
@@ -17,6 +31,21 @@ export default function List() {
           <h4 className="font-bold text-lg">
             {name} ${price}
           </h4>
+          <button
+            onClick={() => {
+              handleAmountChange(i, -1);
+            }}
+          >
+            -
+          </button>
+          <span>{amount}</span>
+          <button
+            onClick={() => {
+              handleAmountChange(i, 1);
+            }}
+          >
+            +
+          </button>
         </div>
       ))}
     </div>
--- public/util/constant.ts
+++ public/util/constant.ts
@@ -1,19 +1,23 @@
 interface Product {
   name: string;
   price: number;
+  amount: number;
 }
 export const PRODUCT_LIST: Product[] = [
   {
     name: "Tomatoes",
     price: 40,
+    amount: 0,
   },
   {
     name: "Pasta",
     price: 50,
+    amount: 0,
   },
   {
     name: "Coconut",
     price: 70,
+    amount: 0,
   },
 ];
 
